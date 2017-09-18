3fc138630584bfa7b7a9b6bd2ed8d260cd3c758e
Gonzalo Iniguez <gonza.ini94@gmail.com>
2017-09-18 15:04:42 -0300

    Agregue un personaje nuevo y cambie los basurero por frutas

--- src/Costanera.ts
+++ src/Costanera.ts
@@ -2,26 +2,18 @@
 
 class Costanera
 {
-	
-
 	game:Phaser.Game;
 	ancho: number;
 	alto:number;
-	doblesalto:number;
 	personaje: Phaser.Sprite;
+	obstaculo: Phaser.Sprite;
 	cursores:Phaser.CursorKeys;
 	saltarBtn:Phaser.Key;
-		
+	facing: string;
+	emitter: Phaser.Particles.Arcade.Emitter;
+	
 
 //--------------------setters y getters --------------------------------------
-
-	getDobleSalto(){
-		return this.doblesalto;	
-	}
-	setDobleSalto(value:number){
-		this.doblesalto = value;
-	}
-
 	setGame(game: Phaser.Game ){
 		this.game = game;
 	}
@@ -54,6 +46,14 @@ class Costanera
 		return this.personaje;
 	}
 
+	setObstaculo(value: Phaser.Sprite ){
+		this.obstaculo = value;
+	}
+
+	getObstaculo (){
+		return this.obstaculo;
+	}
+
 	setCursores(cursores: Phaser.CursorKeys ){
 		this.cursores = cursores;
 	}
@@ -70,6 +70,22 @@ class Costanera
 		return this.saltarBtn;
 	}
 
+	setFacing(facing: string){
+		this.facing = facing;
+	}
+
+	getFacing(){
+		return this.facing;
+	}
+
+	setEmitter(value: Phaser.Particles.Arcade.Emitter){
+		this.emitter = value
+	}
+
+	getEmitter(){
+		return this.emitter;
+	}
+
 
 
 
@@ -81,7 +97,7 @@ class Costanera
 		// Phaser.AUTO - determine the renderer automatically (canvas, webgl)
 		// 'content' - the name of the container to add our game to
 		// { preload:this.preload, create:this.create} - functions to call for our states
-		this.setGame(new Phaser.Game( ancho, alto, Phaser.CENTER, 'content', { 
+		this.setGame(new Phaser.Game( ancho, alto, Phaser.CANVAS, 'content', { 
 			preload:this.preload, 
 			create:this.create, 
 			update: this.update,
@@ -93,13 +109,18 @@ class Costanera
 			getAlto: this.getAlto,
 			setPersonaje: this.setPersonaje,
 			getPersonaje: this.getPersonaje,
+			setObstaculo: this.setObstaculo,
+			getObstaculo: this.getObstaculo,
 			setCursores: this.setCursores,
 			getCursores: this.getCursores,
 			setSaltarBtn: this.setSaltarBtn,
 			getSaltarBtn: this.getSaltarBtn,
-			getDobleSalto: this.getDobleSalto,
-			setDobleSalto: this.setDobleSalto,
-			
+			getFacing: this.getFacing,
+			setFacing: this.setFacing,
+			getEmitter: this.getEmitter,
+			setEmitter: this.setEmitter,
+			collisionHandler: this.collisionHandler,
+			listener: this.listener
 		} ));
 	}
 	
@@ -108,8 +129,9 @@ class Costanera
 		// add our logo image to the assets class under the
 		// key 'logo'. We're also setting the background colour
 		// so it's the same as the background colour in the image
-		this.getGame().load.image('player', 'assets/personajep.png');
-		this.getGame().load.image( 'costanera', "assets/costanera.jpg");
+		this.getGame().load.image('obstaculo', 'assets/manzana.png');
+		this.getGame().load.spritesheet('player', 'assets/Personaje.png', 36.5, 48);
+		this.getGame().load.image( 'costanera', "assets/costanera.jpg" );
 		
 		//Agregamos un comentario para probar subir cambios a GIT desde el editor
 		//hacemos un cambio en el archivo
@@ -131,52 +153,121 @@ class Costanera
 		logo.height = this.getGame().height;
 		logo.width = this.getGame().width;
 
+		this.getGame().physics.startSystem(Phaser.Physics.ARCADE);
+		this.getGame().time.desiredFps = 30;
+
+		this.getGame().physics.arcade.gravity.y = 250;
+
 		var personaje = this.getGame().add.sprite(100, 200, 'player');
-		personaje.height = 150;
-		personaje.width = 75;
+		personaje.height = 200;
+		personaje.width = 100;
 		this.setPersonaje(personaje);
 		
-		this.getGame().physics.arcade.enable(this.getPersonaje());
-		
+		this.getGame().physics.enable(this.getPersonaje(),Phaser.Physics.ARCADE);
+				
+		//Personaje
 		this.getPersonaje().body.collideWorldBounds = true;
 		this.getPersonaje().body.gravity.y = 500;
+		this.getPersonaje().body.setSize(20, 36.5, 5, 16);
+		this.getPersonaje().animations.add('left', [9, 10, 11, 12,13,14], 10, true);
+		this.getPersonaje().animations.add('turn', [4], 20, true);
+		this.getPersonaje().animations.add('right', [1, 2, 3, 4,5,6], 10, true);
+		this.setFacing('left');
+
+		//obstaculo
+		var obstaculo = this.getGame().add.sprite(300, 50, 'obstaculo');
+		this.setObstaculo(obstaculo);
+		obstaculo.name = 'obstaculo';
+	
+		this.getGame().physics.enable(obstaculo, Phaser.Physics.ARCADE);
+		logo.inputEnabled = true;
+		logo.events.onInputDown.add(this.listener, this);
+		//this.getObstaculo().body.velocity.y = 10;
+	
+		//  This adjusts the collision body size.
+		//  220x10 is the new width/height.
+		//  See the offset bounding box for another example.
+		this.getObstaculo().body.setSize(10, 10, 0, 0);
 		
 		this.setCursores(this.getGame().input.keyboard.createCursorKeys());
 		this.setSaltarBtn(this.getGame().input.keyboard.addKey(Phaser.Keyboard.SPACEBAR));
-				
+
+		//emitter
+		var emitter = this.getGame().add.emitter(this.getGame().world.centerX, 5, 5);
+		this.setEmitter(emitter);
+		this.getEmitter().width = this.getGame().world.width;
+
+		this.getEmitter().makeParticles('obstaculo',null,1,true);
+		// emitter.minParticleScale = 0.1;
+		// emitter.maxParticleScale = 0.5;
+	
+		this.getEmitter().setYSpeed(100, 200);
+		this.getEmitter().setXSpeed(-5, 5);
+		
+		this.getEmitter().start(false, 1600, 1, 0);
 	}
 
 	
 	update () {
 		
 			// this.game.physics.arcade.collide(this.player, platforms);
-		
+			//this.getGame().physics.arcade.collide(this.getObstaculo(), this.getPersonaje(), this.collisionHandler, null, this);
+			this.getGame().physics.arcade.collide(this.getEmitter(),this.getPersonaje(),this.collisionHandler,null, this);
 			this.getPersonaje().body.velocity.x = 0;
 		
 			if (this.getCursores().left.isDown)
 			{
-				this.getPersonaje().body.velocity.x = -250;
+				this.getPersonaje().body.velocity.x = -500;
+				if (this.getFacing() != 'left'){
+						this.getPersonaje().animations.play('left');
+						this.setFacing('left');
+				}
 			}
-			else if (this.getCursores().right.isDown)
-			{
-				this.getPersonaje().body.velocity.x = 250;
+			else if (this.getCursores().right.isDown){
+				this.getPersonaje().body.velocity.x = 500;
+				if (this.getFacing() != 'right'){
+						this.getPersonaje().animations.play('right');
+						this.setFacing('right');
+				}
+			} else {
+				if (this.getFacing() != 'idle'){
+						this.getPersonaje().animations.stop();
+			
+						if (this.getFacing() == 'left'){
+							this.getPersonaje().frame = 8;
+						}
+						else{
+							this.getPersonaje().frame = 7;
+						}
+						this.setFacing('idle')
+				}
 			}
 		
-			if (this.getSaltarBtn().isDown && this.getPersonaje().body.onFloor())
+			if (this.getSaltarBtn().isDown && (this.getPersonaje().body.onFloor()))
 			{
-				this.getPersonaje().body.velocity.y = -400;
-				this.setDobleSalto(1);
-				this.getSaltarBtn().isDown = false;
-				console.log(this.getSaltarBtn(), "Primer Salto");
+				this.getPersonaje().body.velocity.y = -800;
 			}
-			if(this.getSaltarBtn().isDown && this.getDobleSalto()==1)
-			{
-				this.getPersonaje().body.velocity.y = -400;
-				this.setDobleSalto(2);
-				this.getSaltarBtn().isDown = false;				
-				console.log(this.getDobleSalto, "Segundo salto");				
+	}
+
+	collisionHandler (objetos, personaje) {
+		
+			// this.getGame().stage.backgroundColor = '#992d2d';
+			// this.getPersonaje().body.velocity.y = -800;
+			objetos.kill();
+			if(objetos.body.onFloor()){
+				personaje.kill();				
 			}
+			personaje.revive();
+		
 		}
+
+		
+		listener () {
+			this.getPersonaje().revive()
+			
+		}
+
+	
 }
 
 // when the page has finished loading, create our game
